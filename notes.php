9fcc2e131beaf34dceff21513ff29fef4bab4d70
antonioArauj <antoniogaraujo63@gmail.com>
2024-09-12 12:39:29 -0300

    Aula EXTRA

--- notes.php
+++ notes.php
@@ -22,3 +22,22 @@
     Já tinha conhecimento sobre o assunto, sem anotações e exercícios
 
 */
+
+
+/*
+ AULA EXTRA - APROFUNDAMENTO TEORICO E BOAS PRATICAS NO USO DE VARIAVEIS
+
+ $nome = 'Antonio'
+       =
+ (Prefixo)Chave = VALOR
+
+ BOAS PRÁTICAS
+
+ - Usar camelCase ou uder_score e ser consistente
+ - Evite nomes genéricos, ex: $x = 'Antonio'; $y = 'Araujo';
+ - Escolha nomes de variaveis que indiquem PROPÓSITO do valor
+ - Evite nomes muito longos(A menos que seja necessário p/ser descritivo):
+      $nomeDoUsuarioLogado é RUIM
+      mas $nomeUsuario é BOM
+
+*/
