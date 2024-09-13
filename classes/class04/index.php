5f521171d8a3fc306789316064fcea2c186bf87a
antonioArauj <antoniogaraujo63@gmail.com>
2024-09-13 11:05:34 -0300

    aula 04 - domine as strings no PHP

---
+++ classes/class04/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+EXERCICIO PASSADO NA AULA. 
+
+IMPRIMIR O TEXTO:
+"MEU NOME É: (NOME), ele tem (X) caracteres"
+*/
+
+$name = 'Antonio';
+$characters = strlen($name);
+
+echo 'Meu nome é: '.$name.', ele tem '.$characters.' caracteres.'; 
+
+echo '<br />';
+
+//RESPOSTA DO PROFESSOR
+ echo "Meu nome é: $name, ele tem $characters caracteres"; 
+ 
---
+++ classes/class04/notes.md
@@ -0,0 +1,22 @@
+
+
+exemplo de string: 
+$nome = 'Antonio';
+
+Exibir: 
+echo $nome;
+
+Concatenar
+echo $nome.$nome
+
+Exibir junto com string:
+echo "Meu nome é: $nome"; 
+
+Contar caracteres
+ strlen($nome); 
+
+
+ EXERCICIO PASSADO NA AULA. 
+
+IMPRIMIR O TEXTO:
+"MEU NOME É: (NOME), ele tem (X) caracteres"
