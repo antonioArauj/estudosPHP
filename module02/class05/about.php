6ec0263dbfb879dbb8cb908c68f07fe9a5299ff6
antonioArauj <antoniogaraujo63@gmail.com>
2024-10-17 18:34:49 -0300

    AULA 05 - Componetizando seu código

---
+++ module02/class05/about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SOBRE NÓS</title>
+    <link rel="stylesheet" href="./styles.css">
+</head>
+
+<body>
+    <header>
+        <?php include './components/header.php'?> 
+    </header>
+    <main>
+        <section>
+            <h2>Sobre nós</h2>
+            <p>
+                Lorem, ipsum dolor sit amet
+                consectetur adipisicing elit
+            </p>
+        </section>
+    </main>
+    <footer>
+        <?php include './components/footer.php'?>
+    </footer>
+</body>
+</html>
---
+++ module02/class05/notes.md
@@ -0,0 +1,4 @@
+INCLUDE - Inclui um arquivo se o arquivo existir, caso contrário, exibe um erro do tipo WARNING e continua a execução do código, normalmente utilizado quando a inclusão do arquivo for opcional. 
+
+REQUIRE - inclui um arquivo se o arquivo existir, caso contrário, exibe um erro do tipo FATAL e para a interrompe a execução do código, normalmente utilizado qaundo o código NÃO PODE continuar sem aquele arquivo. 
+
