496d21bb92a079eb863f916e481dc0c43ca10136
antonioArauj <antoniogaraujo63@gmail.com>
2024-10-17 10:32:52 -0300

    feat/inserting database simulator

---
+++ database.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'users' => [
+        [
+            'name' => 'João Silva',
+            'age' => 20,
+            'sex' => 'M',
+            'monthlySalary' => 5500.03,
+            'annualSalary' => null,
+            'jobStatus' => true,
+            'yearsToRetire' => null,
+            'skills' => 'PHP, Banco de Dados, Vue'
+        ],
+        [
+            'name' => 'Ana Paula',
+            'age' => 30,
+            'sex' => 'F',
+            'monthlySalary' => 10000.0,
+            'annualSalary' => null,
+            'jobStatus' => true,
+            'yearsToRetire' => null,
+            'skills' => 'React, Angular, Vue'
+        ],
+        
+    ]
+];
